d9eda00dedb78620cf967db54078fe3db59719e3
sukhpreetkaur27 <sukhpreetkaur27@ymail.com>
2024-12-20 18:44:25 -0500

    Update SymmetricTree.java

--- SymmetricTree.java
+++ SymmetricTree.java
@@ -1,3 +1,8 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
 // LC 101
 public class SymmetricTree {
     public class TreeNode {
@@ -28,7 +33,7 @@ public class SymmetricTree {
      * @param root
      * @return
      */
-    public boolean isSymmetric(TreeNode root) {
+    public boolean isSymmetric_dfs(TreeNode root) {
         return isSymmetric(root.left, root.right);
     }
 
@@ -44,5 +49,46 @@ public class SymmetricTree {
             return isSymmetric(p.left, q.right) && isSymmetric(p.right, q.left);
         }
         return false;
+    }
+
+    /**
+     * BFS == Compare Pairs
+     * <p>
+     * TC: O(n)
+     * SC: O(nodes in the last level)
+     *
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric_bfs(TreeNode root) {
+        // NOTE: doesn't work with ArrayDeque as Deque doesn't allow null values
+        // So, use Queue with LinkedList
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root.left);
+        queue.offer(root.right);
+        while (!queue.isEmpty()) {
+            // compare pairs
+            TreeNode p = queue.poll();
+            TreeNode q = queue.poll();
+            if (p == null && q == null) {
+                // fine == symmetric
+                continue;
+            }
+            if (p != null && q != null) {
+                if (p.val != q.val) {
+                    // not symmetric
+                    return false;
+                }
+                // compare left and right subtrees i.e. opposite trees of p and q
+                // add alternate pairs to compare
+                queue.offer(p.left);
+                queue.offer(q.right);
+                queue.offer(p.right);
+                queue.offer(q.left);
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
 }
